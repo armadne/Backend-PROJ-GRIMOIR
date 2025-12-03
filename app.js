d7024402bd6c19139e62b9b5e357c9942b51435f
armadne <madane.aroul@hotmail.com>
2025-12-03 23:26:45 +0100

    optimisation de la structure backend

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-const Book = require('./models/Book');
+const stuffRoutes = require('./routes/book');
 
 
 mongoose.connect('mongodb+srv://grimoir:grimoir@cluster-grimoir.xfqkilm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-Grimoir',
@@ -12,43 +13,11 @@ mongoose.connect('mongodb+srv://grimoir:grimoir@cluster-grimoir.xfqkilm.mongodb.
 
 const app = express();
 
+
 app.use(express.json());
 
+app.use('/api/stuff', stuffRouter);
 
 
-app.post('/api/stuff', (req, res, next) => {
-  delete req.body._id;
-  const book = new Book({
-    ...req.body
-  });
-  book.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.put('/api/stuff/:id', (req, res, next) => {
-  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.delete('/api/stuff/:id', (req, res, next) => {
-  Book.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.get('/api/stuff/:id', (req, res, next) => {
-  Book.findOne({ _id: req.params.id })
-    .then(thing => res.status(200).json(thing))
-    .catch(error => res.status(404).json({ error }));
-});
-
-app.get('/api/stuff', (req, res, next) => {
-
-  Book.find()
-    .then(things => res.status(200).json(things))
-    .catch(error => res.status(400).json({ error }));
-});
 
 module.exports = app;
