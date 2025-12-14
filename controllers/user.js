312453225144149b676bf43d3dd17d2e2c3fd00e
armadne <madane.aroul@hotmail.com>
2025-12-14 16:53:41 +0100

    "#"

--- controllers/user.js
+++ controllers/user.js
@@ -28,14 +28,17 @@ exports.login = (req, res, next) => {
                        return res.status(401).json({ error: 'Mot de passe incorrect !' });
                    }
                    res.status(200).json({
-                       userId: user._id,
-                            token: jwt.sign(
-            { userId: user._id, role: user.role },
-            process.env.JWT_SECRET,
-            { expiresIn: '24h' }
-        )
 
-                   });
+  userId: user._id,
+  role: user.role, 
+  token: jwt.sign(
+    { userId: user._id, role: user.role },
+    process.env.JWT_SECRET,
+    { expiresIn: '24h' }
+  )
+});
+
+
                })
                .catch(error => res.status(500).json({ error }));
        })
--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 
-  role: { type: String, default: "admin" }
+  role: { type: String, default: "user" } 
 });
 
 userSchema.plugin(uniqueValidator);
