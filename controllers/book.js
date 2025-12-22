94f8d473b2f709b7aa1beab3237d4e87e4d08930
armadne <madane.aroul@hotmail.com>
2025-12-22 20:12:52 +0100

    "#"

--- controllers/book.js
+++ controllers/book.js
@@ -74,7 +74,7 @@ exports.deleteBook = (req, res, next) => {
    Book.findOne({ _id: req.params.id})
        .then(book => {
            if (book.userId != req.auth.userId) {
-               res.status(401).json({message: 'Not authorized'});
+               res.status(401).json({message: 'Non autorisé'});
            } else {
                const filename = book.imageUrl.split('/images/')[1];
                fs.unlink(`images/${filename}`, () => {
